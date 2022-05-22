d4998431265d74cab83823db63ffa87ff8cdb212
Lesitsa <alesyagugaeva@mail.ru>
2022-05-22 14:26:03 +0300

    Рендеринг JSX

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [mess, setmess] = useState(-1);
+    const callBack = (message: number) => {
+        return setmess(message);
+    };
+    useEffect(() => {
+        subscribe(props.sourceId, callBack);
+        setmess(-1);
+        return () => {
+            unsubscribe(props.sourceId, callBack);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {mess}
+        </div>
+    );
 }
